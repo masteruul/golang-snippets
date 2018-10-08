3faa076a2195cb6f8494988ce9a9c75e0b8b05d6
syamsulhuda <syamsul.huda@tokopedia.com>
2018-10-08 18:49:33 +0700

    update senin 7 oktober 2018

---
+++ go-hit-tome/app.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"bytes"
+	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+)
+
+type HeaderCustom struct {
+	Authorization string `json:"Authorization"`
+	ContentType   string `json:"Content-Type"`
+	Origin        string `json:"Origin"`
+	Referer       string `json:"Referer"`
+	XDevice       string `json:"X-Device"`
+}
+
+type Product struct {
+	ProductID    int `json:"product_id"`
+	ProductStock int `json:"product_stock"`
+	ProductPrice int `json:"product_price"`
+}
+
+type BodyCustom struct {
+	ProductList []Product `json:"product_list"`
+	Sender      string    `json:"sender"`
+}
+
+func main() {
+	//add client api
+	server := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "Hello")
+	}))
+	defer server.Close()
+	client := http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: true,
+			},
+		},
+	}
+
+	fmt.Println("access tome API")
+	method := "PATCH"
+	endpointTome := "https://tome-staging.tokopedia.com/v2/batch/product/"
+	h := HeaderCustom{
+		Authorization: "Bearer HsuEIJ97SoKwKn2fNNsgrQ",
+		ContentType:   "application/json",
+		Origin:        "https://www.tokopedia.com",
+		Referer:       "https://www.tokopedia.com",
+		XDevice:       "IOS",
+	}
+
+	// p := Product{ProductID: 15223032, ProductStock: 250, ProductPrice: 750000}
+	body := BodyCustom{
+		Sender: "fulfillment_service",
+		ProductList: []Product{
+			Product{ProductID: 15223032, ProductStock: 250, ProductPrice: 750000},
+			Product{ProductID: 15223032, ProductStock: 250, ProductPrice: 750000},
+		},
+	}
+
+	tbody, err := json.Marshal(body)
+	if err != nil {
+		fmt.Println("err: ", err)
+	}
+	req, err := http.NewRequest(method, endpointTome, bytes.NewReader([]byte(tbody)))
+	if err != nil {
+		fmt.Println("has error", err)
+	}
+	req.Header.Add("Authorization", h.Authorization)
+	req.Header.Add("Content-Type", h.ContentType)
+	req.Header.Add("Origin", h.Origin)
+	req.Header.Add("Referer", h.Referer)
+	req.Header.Add("X-Device", h.XDevice)
+	fmt.Println("Header", req.Header)
+	response, err := client.Do(req)
+	if err != nil {
+		fmt.Printf("HTTP request failed with error %s\n", err)
+	} else {
+		data, _ := ioutil.ReadAll(response.Body)
+		fmt.Printf(string(data))
+	}
+}
