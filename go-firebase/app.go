edcb6f1068a2bdce92551b7ed87f384bdfa625a8
syamsulhuda <syamsul.huda@tokopedia.com>
2018-10-09 14:01:25 +0700

    setup firebase

--- go-firebase/app.go
+++ go-firebase/app.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"golang.org/x/net/context"
+
+	firebase "firebase.google.com/go"
+
+	"google.golang.org/api/option"
+)
+
+func main() {
+	opt := option.WithCredentialsFile("path/to/serviceAccountKey.json")
+	app, err := firebase.NewApp(context.Background(), nil, opt)
+	if err != nil {
+		log.Fatalf("error initializing app: %v\n", err)
+	}
+	log.Println("Golang + Firebase running", app)
 }
